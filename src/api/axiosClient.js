fd5001aa34076e81fdc5750ef94a72fe68d78186
Fernando Fernández <ferferga@hotmail.com>
2025-04-22 01:45:43 +0200

    feat: reactively apply axios interceptors
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,17 +1,12 @@
-import { createAxiosClient } from '@/api/createAxiosClient';
-import { signOut } from '@/services/auth';
+import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3001/api/v1';
 
-
-export const client = createAxiosClient({
-  options: {
-    withCredentials: true,
-    baseURL: BASE_URL,
-    timeout: 300000,
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  },
-  logout: signOut,
+export const client = axios.create({
+  withCredentials: true,
+  baseURL: BASE_URL,
+  timeout: 300000,
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
