208e9ab2816a54e1cb3675fbfb966466cc85aba7
Fernando Fernández <ferferga@hotmail.com>
2025-04-22 01:45:43 +0200

    feat: reactively apply axios interceptors
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- src/api/nodeRedClient.js
+++ src/api/nodeRedClient.js
@@ -1,14 +1,11 @@
-import { createAxiosClient } from '@/api/createAxiosClient';
+import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_NODE_RED_URL || 'http://localhost:1880';
 
-export const nodeRedClient = createAxiosClient({
-  options: {
-    baseURL: BASE_URL,
-    timeout: 300000,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${localStorage.getItem('token')}`
-    }
+export const nodeRedClient = axios.create({
+  baseURL: BASE_URL,
+  timeout: 300000,
+  headers: {
+    'Content-Type': 'application/json'
   }
 });
