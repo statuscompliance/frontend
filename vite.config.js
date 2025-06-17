7b6ded6782190edce7cf473c781bf536b8fbdbd2
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2025-06-17 18:35:15 +0200

    fix(nginx): enable path rewriting for /backend proxy

--- vite.config.js
+++ vite.config.js
@@ -25,16 +25,21 @@ export default defineConfig({
   server: {
     proxy: {
       '/backend': {
+        // eslint-disable-next-line
         target: process.env.VITE_BASE_URL || 'http://127.0.0.1:3001/api/v1',
-        changeOrigin: true
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/backend/, '')
       },
       '/node-red': {
+        // eslint-disable-next-line
         target: process.env.VITE_NODE_RED_URL || 'http://127.0.0.1:1880',
         changeOrigin: true
       },
       '/grafana': {
+        // eslint-disable-next-line
         target: process.env.VITE_GRAFANA_URL || 'http://127.0.0.1:3100',
-        changeOrigin: true
+        changeOrigin: true,
+        // rewrite: path => path.replace(/^\/grafana/, '')
       }
     }
   }
