73946fbb65c35a4280e17d71a051eba86d5acb93
Fernando Fernández <ferferga@hotmail.com>
2025-06-20 16:26:18 +0000

    chore(eslint): fix env for node-based files

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      /* global __dirname */
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(import.meta.dirname, './src'),
     },
   },
   css: {
@@ -25,18 +24,15 @@ export default defineConfig({
   server: {
     proxy: {
       '/backend': {
-        // eslint-disable-next-line
         target: process.env.VITE_BASE_URL || 'http://127.0.0.1:3001/api/v1',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/backend/, '')
       },
       '/node-red': {
-        // eslint-disable-next-line
         target: process.env.VITE_NODE_RED_URL || 'http://127.0.0.1:1880',
         changeOrigin: true
       },
       '/grafana': {
-        // eslint-disable-next-line
         target: process.env.VITE_GRAFANA_URL || 'http://127.0.0.1:3100',
         changeOrigin: true,
         // rewrite: path => path.replace(/^\/grafana/, '')
